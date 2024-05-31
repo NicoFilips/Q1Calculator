4081045c2a3f0f83e9d624d86b24e9d896dd6136
NicoFilips <git@nicofilips.de>
2024-05-31 16:04:18 +0200

    feat(MainLayout): Frontend

--- src/Frontend/fe_Q1Calculator/Components/Calculator/InputNumber.razor.cs
+++ src/Frontend/fe_Q1Calculator/Components/Calculator/InputNumber.razor.cs
@@ -2,6 +2,6 @@ using Microsoft.AspNetCore.Components;
 
 namespace fe_Q1Calculator.Components.Calculator;
 
-public partial class Calculate : ComponentBase
+public partial class InputNumber : ComponentBase
 {
 }
